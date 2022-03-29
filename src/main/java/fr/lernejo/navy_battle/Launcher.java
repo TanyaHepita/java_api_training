4f02cef80dac62bdae193779a1134418240ed96a
tanya haliche <tanya.haliche@epita.fr>
2022-03-29 18:29:31 +0200

    Partie 2 - Serveur HTTP (1)

--- src/main/java/fr/lernejo/navy_battle/Launcher.java
+++ src/main/java/fr/lernejo/navy_battle/Launcher.java
@@ -1,7 +1,26 @@
 package fr.lernejo.navy_battle;
+import java.io.IOException;
+
 
 public class Launcher {
-    public static void main() {
+    
+    public static void main(String[] args) throws IOException {
+        
+        if (args.length <1 || args.length>2) 
+        {
+            
+            throw new IllegalArgumentException("Port utilisé");
+        
+        }
+        if  (args.length==1) 
+
+        {
+            MyHttpServer httpserver = new MyHttpServer(args[0]);
+            httpserver.createServer();
+        }
+
+    
     }
+
 }
 
---
+++ src/main/java/fr/lernejo/navy_battle/PingHandler.java
@@ -0,0 +1,24 @@
+package fr.lernejo.navy_battle;
+
+import com.sun.net.httpserver.HttpHandler;
+
+import com.sun.net.httpserver.HttpExchange;
+import java.io.IOException;
+
+import java.io.OutputStream;
+
+public class PingHandler implements HttpHandler {
+    @Override
+
+    public void handle(HttpExchange exchange) throws IOException {
+        exchange.sendResponseHeaders(200,2); 
+
+        try (OutputStream os = exchange.getResponseBody()) {
+            
+            os.write("ok ! ".getBytes());
+        }
+
+
+    }
+}
+
