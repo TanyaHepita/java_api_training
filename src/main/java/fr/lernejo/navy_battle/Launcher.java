8d0abc074959ef34656c4e381dbebaef93c5cab8
tanya haliche <tanya.haliche@epita.fr>
2022-03-30 09:23:12 +0200

    Partie 2 - Serveur HTTP(3)

--- src/main/java/fr/lernejo/navy_battle/Launcher.java
+++ src/main/java/fr/lernejo/navy_battle/Launcher.java
@@ -1,43 +1,18 @@
 package fr.lernejo.navy_battle;
-
-
 import java.io.IOException;
 
 public class Launcher {
 
-    public static void main(String[] args) throws IOException, InterruptedException 
-
-    {
-        if(args.length != 2 && args.length != 1) 
-        {
-          
-            System.err.println("Main usage: port or port url");
-        }
-        else if(args.length==1)
-        {
-        
-            HttpServerSimple server = new HttpServerSimple(Integer.parseInt(args[0]), "");
-            
-            server.start();
-        }
-        else  
-        {
-            HttpServerSimple server2 = new HttpServerSimple(Integer.parseInt(args[0]), args[1]);
-            
-            server2.start();
-            
-            HttpClientStart client = new HttpClientStart(Integer.parseInt(args[0]));
-        
-            client.SendStart(args[1]);
-
-        }
+    private static int port = 9876;
+    private static final int HTTP_OK_STATUS = 200;
 
 
-
+    public static void main(String[] args) throws IOException {
+        // Create server
+        HttpServerSimple server = new HttpServerSimple(port, new HttpHandlerSimple());
+        // Start
+        server.start();
     }
-
 }
-
-
 
 
